b8f1d9a682a916c9c49713b4800f8668ba15a007
ihysk <sirouma.09@gmail.com>
2017-10-18 23:45:32 +0900

    Updated hello world

--- 01_getting-started/01_helloWorld/main.go
+++ 01_getting-started/01_helloWorld/main.go
@@ -1,7 +1,11 @@
+// package mainではmain関数を定義することでエントリポイントになる
 package main
 
+// importすると、packageのメンバが使用可能になる
+// フォーマットI/O C言語のprintfおよびscanf的なやつ
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+    // 大文字のメンバはimportで利用可能になる
+	fmt.Println("Hello ihysk!")
 }
